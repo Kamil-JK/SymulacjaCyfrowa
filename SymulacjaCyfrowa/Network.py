774926e8c2bfabc75299dade814701f90eaff323
unknown <Student@LAB4V2K6.et.put.poznan.pl>
2023-03-23 14:57:37 +0100

    Commit3

--- SymulacjaCyfrowa/Network.py
+++ SymulacjaCyfrowa/Network.py
@@ -1,6 +1,18 @@
-
+import queue
 
 class Network:
 
+    maxUserNumber = 60
+    distanceBS = 5000
+    userStartPosition = 2000
+    userList = []
+    userBuffer = queue.Queue()
+    
     def __init__(self):
         print("Network created")
+        
+    def createUser():
+        print("User created with speed v")
+    
+    def destroyUser():
+        print("")
